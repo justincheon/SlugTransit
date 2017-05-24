23609b12c71beb0e4da25a32a5c290a71ee45e6f
Shao-Hung Hsiao <shaohungh@gmail.com>
2017-05-24 02:20:39 -0700

    toString

--- timeslot.py
+++ timeslot.py
@@ -1,18 +1,19 @@
 import datetime
 
+
 class timeline:
     def __init__(self):
         self.timetable = {}
 
     def setMode(self, mode):
         if "MWF" in mode:
-            second = 7 * 60 * 60 + 21 * 60 # 7:21 AM
+            second = 7 * 60 * 60 + 21 * 60  # 7:21 AM
             while second < 86400:
                 t = timeslot()
                 self.timetable.update({second: t})
                 second += 13 * 60
         elif "TuTh" in mode:
-            second = 7 * 60 * 60 + 22 * 60 # 7:21 AM
+            second = 7 * 60 * 60 + 22 * 60  # 7:22 AM
             while second < 86400:
                 t = timeslot()
                 self.timetable.update({second: t})
@@ -30,10 +31,23 @@ class timeline:
                     break
             prevtime = time
 
+    def __str__(self):
+        str1 = "{:5} {:6} {:6}\n".format("Time", "CW", "CCW")
+        for time, timeslot in self.timetable.items():
+            m, s = divmod(time, 60)
+            h, m = divmod(m, 60)
+            str1 += "{:02d}:{:02d} {}\n".format(h, m, timeslot)
+        return str1
+
+
 class timeslot:
-    def __init__(self): # CW = 1 CCW = 2
+    def __init__(self):  # CW = 1 CCW = 2
         self.CW = []
         self.CCW = []
+
+    def __str__(self):
+        return "{:06.2f} {:06.2f}".format(self.getAverage_CW(),
+                                      self.getAverage_CCW())
 
     def addCW(self, value):
         self.CW.append(value)
@@ -42,7 +56,16 @@ class timeslot:
         self.CCW.append(value)
 
     def getAverage_CW(self):
+        if len(self.CW) == 0:
+            return 0
         return sum(self.CW) / len(self.CW)
 
     def getAverage_CCW(self):
+        if len(self.CCW) == 0:
+            return 0
         return sum(self.CCW) / len(self.CCW)
+
+
+tl = timeline()
+tl.setMode("MWF")
+print(tl)
