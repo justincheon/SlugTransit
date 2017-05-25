c59252a7a6308df8b21aa22a91099464b1330b8b
Shao-Hung Hsiao <shaohungh@gmail.com>
2017-05-24 23:58:29 -0700

    test.py now have to ability to fix glitch cause by the bug in bus

---
+++ test.py
@@ -0,0 +1,17 @@
+import bus
+
+file = open("loop_log_2017_05_17", "r")
+buses = {}
+for line in file:
+    aline = line.split(" ")
+    if aline[0] not in buses:
+        new = bus.Bus(aline[0])
+        buses.update({aline[0]:new})
+    if aline[1] != buses[aline[0]].getMostRecentLocation():
+        buses[aline[0]].addLog(aline[1], aline[2], aline[3], aline[4])
+        buses[aline[0]].checkDirection()
+        buses[aline[0]].getMostRecentLocationObj().direction = buses[aline[0]].getDirection()
+    
+for key, bus in buses.items():
+    bus.printReport()
+    #bus.printLogs()
